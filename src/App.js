ad127447c28787dfe1196e6b2efa0a1a230b2bbc
Kakashi vp <ngoduong37na@gmail.com>
2021-08-28 10:01:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React,{useState} from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 function App() {
   const [todoList ,settodoList] = useState([
 { id: 1, title: 'I love Easy Frontend! 😍 ' },
 { id: 2, title: 'We love Easy Frontend! 🥰 ' },
 { id: 3, title: 'They love Easy Frontend! 🚀 ' },
 ]);
-
 function handleTodoClick(todo) {
   console.log(todo)
   const  index =todoList.findIndex(x=> x.id === todo.id);
@@ -18,9 +18,20 @@ function handleTodoClick(todo) {
   settodoList(newTodoList);
 }
 
+function handleTodoFormSubmit(formValues) {
+  console.log(formValues)
+  const newTodo = {
+    id : todoList.length + 1,
+    ...formValues,
+  }
+  console.log(newTodo)
+  const newTodoList = [...todoList];
+  newTodoList.push(newTodo)
+settodoList(newTodoList)
+}
   return (
     <div className="App">
-
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList onTodoClick={handleTodoClick} todos={todoList} />
     </div>
   );
